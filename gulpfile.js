1a731ace86397308dddcecb15593a9d6f6926677
Simon Gittins <sgittins@gmail.com>
2015-12-31 11:08:26 +1000

    mocha and chai

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,24 @@ gulp.task('tsc', function () {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('.'));
 });
+
+
+
+
+var mocha = require('gulp-mocha');
+var childProcess = require('child_process');
+require('source-map-support').install();
+require('core-js');
+
+gulp.task('watch', function () {
+    var spawnTests = function() {
+        childProcess.spawn('gulp', ['test'], { stdio: 'inherit' });
+    }
+    spawnTests();
+    gulp.watch('**/*.ts', spawnTests);
+});
+
+gulp.task('test', ['tsc'], function () {
+    return gulp.src(['tests/**/*.js'])
+        .pipe(mocha({ reporter: 'spec', ui: 'bdd' }));
+});
